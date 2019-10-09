5373644ef1a68f6750bdadcee13a11a1531a101b
Tim Goll <tim@timgoll.de>
2019-10-09 20:08:01 +0200

    knife consitency and language

--- lua/terrortown/entities/roles/serialkiller/shared.lua
+++ lua/terrortown/entities/roles/serialkiller/shared.lua
@@ -51,19 +51,21 @@ function ROLE:Initialize()
 		LANG.AddToLanguage("English", self.defaultTeam, "TEAM Serial Killers")
 		LANG.AddToLanguage("English", "hilite_win_" .. self.defaultTeam, "THE SK WON")
 		LANG.AddToLanguage("English", "win_" .. self.defaultTeam, "The Serial Killer has won!")
-		LANG.AddToLanguage("English", "info_popup_" .. self.name, [[Now its your turn! Kill them ALL.]])
-		LANG.AddToLanguage("English", "body_found_" .. self.abbr, "This was a Serial Killer...")
+		LANG.AddToLanguage("English", "info_popup_" .. self.name, [[Now it's your turn! Kill them all. Use your tracker to find your targets.
+		Right klicking with your knife throws an explosive grenade.]])
+		LANG.AddToLanguage("English", "body_found_" .. self.abbr, "They were a Serial Killer!")
 		LANG.AddToLanguage("English", "search_role_" .. self.abbr, "This person was a Serial Killer!")
 		LANG.AddToLanguage("English", "ev_win_" .. self.defaultTeam, "The deadly Serial Killer won the round!")
 		LANG.AddToLanguage("English", "target_" .. self.name, "Serial Killer")
 		LANG.AddToLanguage("English", "ttt2_desc_" .. self.name, [[The Serialkiller needs to kill every player and must be the last survivor to win the game.
-He can access his own ([C]) shop and is able to see every player through the walls (as well as he is able to select the Jester from the other players).]])
+He can access his own ([C]) shop and is able to see every player through the walls (as well as he is able to select the jester from the other players).]])
 
 		LANG.AddToLanguage("Deutsch", self.name, "Serienkiller")
 		LANG.AddToLanguage("Deutsch", self.defaultTeam, "TEAM Serienkiller")
 		LANG.AddToLanguage("Deutsch", "hilite_win_" .. self.defaultTeam, "THE SK WON")
 		LANG.AddToLanguage("Deutsch", "win_" .. self.defaultTeam, "Der Serienkiller hat gewonnen!")
-		LANG.AddToLanguage("Deutsch", "info_popup_" .. self.name, [[Jetzt bist du dran! Töte sie ALLE...]])
+		LANG.AddToLanguage("Deutsch", "info_popup_" .. self.name, [[Jetzt bist du dran! Töte alle. Nutze deinen Tracker um deine Ziele zu finden.
+		Rechts klick mit deinem Messer wirft eine explosive Granate.]])
 		LANG.AddToLanguage("Deutsch", "body_found_" .. self.abbr, "Er war ein Serienkiller...")
 		LANG.AddToLanguage("Deutsch", "search_role_" .. self.abbr, "Diese Person war ein Serienkiller!")
 		LANG.AddToLanguage("Deutsch", "ev_win_" .. self.defaultTeam, "Der tötliche Serienkiller hat die Runde gewonnen!")
@@ -76,12 +78,14 @@ end
 if SERVER then
 	-- Give Loadout on respawn and rolechange	
 	function ROLE:GiveRoleLoadout(ply, isRoleChange)
-		ply:GiveEquipmentItem('item_ttt_tracker')
+		ply:GiveEquipmentItem("item_ttt_tracker")
+		ply:GiveEquipmentWeapon("weapon_sk_knife")
 	end
 
 	-- Remove Loadout on death and rolechange
 	function ROLE:RemoveRoleLoadout(ply, isRoleChange)
 		ply:RemoveEquipmentItem('item_ttt_tracker')
+		ply:StripWeapon("weapon_sk_knife")
 	end
 
 	-- just some networking...
