d0a28b581238fdfd49fa204085307fb21388eab8
Blueberryy <36592509+Blueberryy@users.noreply.github.com>
2020-10-06 17:09:44 +0500

    Create serialkiller.lua (#6)

---
+++ lua/terrortown/lang/russian/serialkiller.lua
@@ -0,0 +1,19 @@
+local L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SERIALKILLER.name] = "Серийный убийца"
+L[SERIALKILLER.defaultTeam] = "Команда серийных убийц"
+L["hilite_win_" .. SERIALKILLER.defaultTeam] = "ПОБЕДА СЕРИЙНЫХ УБИЙЦ"
+L["win_" .. SERIALKILLER.defaultTeam] = "Серийный убийца победил!"
+L["info_popup_" .. SERIALKILLER.name] = [[Теперь твоя очередь! Убить их всех. Используйте трекер, чтобы найти свои цели.
+Щёлкнув правой кнопкой мыши с ножом, вы бросите гранату.]]
+L["body_found_" .. SERIALKILLER.abbr] = "Он был серийным убийцей!"
+L["search_role_" .. SERIALKILLER.abbr] = "Этот человек был серийным убийцей!"
+L["ev_win_" .. SERIALKILLER.defaultTeam] = "Смертоностные серийные убийцы выиграли раунд!"
+L["target_" .. SERIALKILLER.name] = "Серийный убийца"
+L["ttt2_desc_" .. SERIALKILLER.name] = [[Серийный убийца должен убить каждого игрока и должен быть последним выжившим, чтобы выиграть игру.
+Он может получить доступ к своему ([C]) магазину и может видеть каждого игрока через стены (а также он может выбирать шута среди других игроков).]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+L["ttt2_role_sk_knife_primary"] = "Атаковать сфокусированного игрока"
+L["ttt2_role_sk_knife_secondary"] = "Бросить гранату встряски"
