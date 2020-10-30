9b64c99c7facab57e4b0821778484e6fca35bc68
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-10-30 21:36:03 +0100

    Serial Killer French translation (#8)
    * Added french translation

---
+++ lua/terrortown/lang/french/serialkiller.lua
@@ -0,0 +1,19 @@
+local L = LANG.GetLanguageTableReference("french")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SERIALKILLER.name] = "Tueur en série"
+L[SERIALKILLER.defaultTeam] = "Team Tueur en série"
+L["hilite_win_" .. SERIALKILLER.defaultTeam] = "LE TUEUR EN SÉRIE A GAGNÉ"
+L["win_" .. SERIALKILLER.defaultTeam] = "Le tueur en série a gagné !"
+L["info_popup_" .. SERIALKILLER.name] = [[Maintenant, c'est à votre tour ! Tuez-les tous. Utilisez votre traqueur pour trouver vos cibles.
+Un clic droit avec votre couteau lance une grenade explosive.]]
+L["body_found_" .. SERIALKILLER.abbr] = "C'était un tueur en série!"
+L["search_role_" .. SERIALKILLER.abbr] = "Cette personne était un tueur en série!"
+L["ev_win_" .. SERIALKILLER.defaultTeam] = "L'impitoyable tueur en série a gagné le round!"
+L["target_" .. SERIALKILLER.name] = "Tueur en série"
+L["ttt2_desc_" .. SERIALKILLER.name] = [[Le tueur en série doit tuer tous les joueurs et être le dernier survivant pour gagner la partie.
+Il peut accéder à son propre ([C]) shop et est capable de voir chaque joueur à travers les murs (aussi il est capable de sélectionner le bouffon parmi les autres joueurs).]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+L["ttt2_role_sk_knife_primary"] = "Attaquer le joueur ciblé"
+L["ttt2_role_sk_knife_secondary"] = "Lance une grenade"
